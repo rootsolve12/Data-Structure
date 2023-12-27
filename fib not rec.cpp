297b142f843d24fde857011317c5fb7e658e8333
rootsolve12 <153415581+rootsolve12@users.noreply.github.com>
2023-12-28 00:46:03 +0530

    Add files via upload

---
+++ fib not rec.cpp
@@ -0,0 +1,19 @@
+#include<stdio.h>    
+int main()    
+{    
+ int n1=0,n2=1,n3,num; 
+ 
+ printf("Number of elements:");    
+ scanf("%d",&num);
+ 
+ printf("\n%d %d",n1,n2);
+ for(int i=2;i<num;i++)
+ {    
+  n3=n1+n2;    
+  printf(" %d",n3);    
+  n1=n2;    
+  n2=n3;    
+ }  
+ 
+  return 0;  
+ }
